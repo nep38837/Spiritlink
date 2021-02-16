4114a8f316814a9326bb20ff87e4c9edd590c108
nep38837 <76069872+nep38837@users.noreply.github.com>
2021-02-16 17:08:01 +0100

    Biphasisches Modell

---
+++ biphasisches_modell02.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="biphasisches_modell02" tilewidth="128" tileheight="128" tilecount="120" columns="15">
+ <image source="biphasisches_modell02.png" width="1920" height="1080"/>
+</tileset>
